8e592e994bbf30ed378794178cc72b8b101f0b91
mymphe <azatsdev@gmail.com>
2021-10-27 13:23:57 +0600

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,26 @@
 import { utils } from "web3";
 import { useWeb3 } from "../web3";
 
+import "./header.css";
+
 const Header = () => {
   const { active, account, balance, connect } = useWeb3();
   const shortAccount = account.slice(0, 4) + "..." + account.slice(-4);
+  const readableBalance = parseFloat(utils.fromWei(balance)).toFixed(3);
 
   return (
     <header>
-      <h1>Donate</h1>
-      {active ? (
-        <div>
-          <p>{shortAccount}</p>
-          <p>{utils.fromWei(balance)}</p>
-        </div>
-      ) : (
-        <button onClick={connect}>Connect wallet</button>
-      )}
+      <div className='content'>
+        <h1>Fund</h1>
+        {active ? (
+          <div>
+            <p>{shortAccount}</p>
+            <p>{readableBalance} ETH</p>
+          </div>
+        ) : (
+          <button onClick={connect}>Connect wallet</button>
+        )}
+      </div>
     </header>
   );
 };
